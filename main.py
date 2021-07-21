6e3341b4adacd606747fe690b33a23cd7f77360e
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2021-07-21 03:15:22 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+dictionary = {'Key' : 'Value'} 
+dictionary2 = {'Boys':{'James':16} , 'Girls':{'Taylor':19}}
+
+dictionary3 = dictionary2.copy() #to copy all values in another dictionary
+
+
+Car = {
+  "brand": "Tesla",
+  "model": "X",
+  "year": 2021,
+"colors": ["red", "white", "blue"]
+}
+
+#to print only a single value in a dictionary
+
+print(Car['brand'])
